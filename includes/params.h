4dfcb1d79124cea42a0fc6eba67a29a517aa420f
marina1177 <marina1177@gmail.com>
2023-11-24 19:22:54 +0300

    start fill coordinates process

--- includes/params.h
+++ includes/params.h
@@ -4,11 +4,20 @@
 # define TRAP_FREQ					50 //[Hz]
 # define FULL_TIME					10*1/TRAP_FREQ
 # define TIME_STEP		 			1/TRAP_FREQ/100
-# define TRAP_U						5000 //[V]
-# define NU_AIR			1/(4*3.14*EPS_CONST)
-# define AMOUNT_OF_PARAMS  10
+# define TRAP_U_ALTERNATIVE			5000 //[V]
+# define TRAP_U_DIRECT			    0//[V]
+# define NU_AIR			            18.27*pow(10,-6)
+# define TRAP_ELECTRODE_DIAMETER    0.004
+# define TRAP_ELECTRODE_DISTANCE    0.019
 
-# define IO_HEADER_OFFSETS  80
+# define PARTICLE_DIAMETER          2*10e-5 //[m]	
+# define PARTICLE_RHO                3.95e3 //[]
+# define PARTICLE_Q                  98000//??[e]
+# define PARTICLE_M                  PARTICLE_RHO *(4/3)*pow(PARTICLE_DIAMETER/2, 3);
 
+// particle->rho = 3.95e3;
+// 	particle->d = 2*10e-5;
+// 	particle->m = particle->rho*(4/3)*M_PI *pow(particle->d/2, 3);
+// 	particle->q = 5e-16;
 #endif
 
--- src/gen_main.c
+++ src/gen_main.c
@@ -1,25 +1,63 @@
 #include "../includes/verle.h"
 
+int check_intersections(double *coords, int i){
+
+//	Find the centre and radius of each sphere.
+	double xc = coords[i*3];
+	double yc = coords[i*3+1];
+	double zc = coords[i*3]+2;
+
+// Find the magnitude of the distance of the line between the sphere's centres 
+	double magnitude = PARTICLE_DIAMETER;
+//If (magnitude distance of line) > radius they do not intersect, 
+//if (magnitude distance of the line) ≤ radius they do intersect.
+
+
+}
+
+double randfrom(double min, double max) 
+{
+    double range = (max - min); 
+    double div = RAND_MAX / range;
+    return min + (rand() / div);
+}
 
 int gen_main(){
 	t_params *params;
+	t_arrv3	*arr;
 
 	//1. создать структуру параметры
 	params = (t_params *) malloc(1 * sizeof(t_params));
 
 	//2. заплнить структуру параметры
-	params->amount_of_particles = ;
-	params->freq = ;
-	params->tfull = ;
-	params->tstep = ;
-	params->U = ;
-	params->nu = ;
-	params->da = ;
-	params->ra = ;
-	params->V = ;
+	params->amount_of_particles = AMOUNT_OF_PARTICLES;
+	params->freq = TRAP_FREQ;
+	params->tfull = FULL_TIME;
+	params->tstep = TIME_STEP;
+	params->U = TRAP_U_ALTERNATIVE;
+	params->nu = NU_AIR;
+	params->da = TRAP_ELECTRODE_DIAMETER;
+	params->ra = TRAP_ELECTRODE_DISTANCE;
+	params->V = TRAP_U_DIRECT;
 
 	//3. создать структуру начальных услвий всех частиц s_array_v3
+	arr = (t_arrv3 *) malloc(1 * sizeof(t_arrv3));
+	//					x y z | vx vy vz |m q r|
+	arr->addr = (t_v3 *) malloc(AMOUNT_OF_PARTICLES*3 * sizeof(t_v3));
+	arr->len = AMOUNT_OF_PARTICLES;
+	arr->size = AMOUNT_OF_PARTICLES;
+
 	//4. заплнить начальные условия координаты с учетом радиуса частиц
+	
+	double coords[AMOUNT_OF_PARTICLES*3];
+	int i = 0;
+	while(i < arr->len){
+		//random for the first particle
+		if(i == 0){
+			
+		}
+		i+=3;
+	}
 	//5. io_write_input - запись параметрв и начальных услвий в файл
 	//6. закрыть файл
 }
